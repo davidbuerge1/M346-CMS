33eb800cd3d622b7fe3f7ccb3d6cd32c32298481
davidbuerge1 <80911657+davidbuerge1@users.noreply.github.com>
2023-12-20 13:56:52 +0100

    Update CMS-server-setup.sh
    added apache2 installation and updated Wordpress installation

--- server-setup/CMS-server-setup.sh
+++ server-setup/CMS-server-setup.sh
@@ -8,16 +8,29 @@ apt-get install docker-ce docker-ce-cli containerd.io -y
 curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-cd /var/www/html
-sudo wget https://wordpress.org/latest.tar.gz
-sudo tar -xvzf latest.tar.gz
-sudo mv wordpress/* .
-sudo rm -rf wordpress latest.tar.gz
+#installation apache, php 
+sudo apt install apache2
+sudo apt install php libapache2-mod-php
+
+
+# Download and extract WordPress
+sudo apt install wget
+cd /tmp
+wget https://wordpress.org/latest.tar.gz
+tar xzvf latest.tar.gz
+sudo cp -a /tmp/wordpress/. /var/www/html
 
 # Set the correct permissions
-sudo chown -R apache:apache /var/www/html
-sudo find /var/www/html -type d -exec chmod 755 {} \;
-sudo find /var/www/html -type f -exec chmod 644 {} \;
+sudo chown -R www-data:www-data /var/www/html
+sudo chmod -R 755 /var/www/html
+
+# Configure Apache
+sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/wordpress.conf
+sudo sed -i 's/\/var\/www\/html/\/var\/www\/html\/wordpress/g' /etc/apache2/sites-available/wordpress.conf
+sudo a2dissite 000-default.conf
+sudo a2ensite wordpress.conf
+sudo a2enmod rewrite
+sudo systemctl restart apache2
 
 cd server-setup
 cd docker
