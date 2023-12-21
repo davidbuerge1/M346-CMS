eab53caa14e3c9e21a939723986d9039c402731f
davidbuerge1 <80911657+davidbuerge1@users.noreply.github.com>
2023-12-21 18:59:55 +0100

    Update CMS-server-setup.sh
    issue fixed

--- server-setup/CMS-server-setup.sh
+++ server-setup/CMS-server-setup.sh
@@ -8,7 +8,7 @@ apt-get install docker-ce docker-ce-cli containerd.io -y
 curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-cd WordPressCMS/server-setup/docker
+cd /WordPressCMS/server-setup/docker
 sed -i "s/<DB-Host>/$1/g" docker-compose.yml
 sed -i "s/<DB-User>/root/g" docker-compose.yml
 sed -i "s/<DB-Password>/$2/g" docker-compose.yml
